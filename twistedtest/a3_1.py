ca5d736b70ef10ef80b31358f47185b232585673
武明辉 <1032662429@qq.com>
2017-08-17 13:54:00 +0800

    FIX: fix some xx

--- twistedtest/a3_1.py
+++ twistedtest/a3_1.py
@@ -4,6 +4,32 @@
 @author: 武明辉 
 @time: 17-8-17 上午11:12
 """
+from sys import stdout
 
-if __name__ == '__main__':
-    pass
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol, ClientFactory
+
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        stdout.write(data)
+
+
+class EchoClientFactory(ClientFactory):
+
+    def startedConnecting(self, connector):
+        print('start to connect')
+
+    def buildProtocol(self, addr):
+        print('connteted')
+        return Echo
+
+    def clientConnectionLost(self, connector, reason):
+        print('Lost connection Reason:', reason)
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print('connection failed. Reason:', reason)
+
+reactor.connectTCP('host', 8123, EchoClientFactory())
+reactor.run()
--- twistedtest/a3_2.py
+++ twistedtest/a3_2.py
@@ -4,6 +4,35 @@
 @author: 武明辉 
 @time: 17-8-17 上午11:28
 """
+from sys import stdout
 
-if __name__ == '__main__':
-    pass
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol, ReconnectingClientFactory
+
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        stdout.write(data)
+
+
+class EchoClientFactory(ReconnectingClientFactory):
+
+    def startedConnecting(self, connector):
+        print('start to connect')
+
+    def buildProtocol(self, addr):
+        print('connteted')
+        print('resetting reconnection delay')
+        self.resetDelay()
+        return Echo
+
+    def clientConnectionLost(self, connector, reason):
+        print('Lost connection Reason:', reason)
+        ReconnectingClientFactory.clientConnectionLost(self,connector,reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print('connection failed. Reason:', reason)
+        ReconnectingClientFactory.clientConnectionFailed(self,connector,reason)
+
+reactor.connectTCP('host', 8123, EchoClientFactory())
+reactor.run()
