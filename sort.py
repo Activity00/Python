7fc58ebacb148c24d3f64e53c032539e1d3dcd9e
Activity00 <1032662429@qq.com>
2018-05-29 10:48:02 +0800

    ADD: select sort

---
+++ sort.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+"""
+@author: 武明辉 
+@time: 2018/5/29 10:06
+"""
+import doctest
+
+
+def select_sort(nums):
+    """
+    >>> select_sort([1, 4, 7, 2, 5, 8])
+    [1, 2, 4, 5, 7, 8]
+    """
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] > nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+    return nums
+
+
+def select_sort_p(nums):
+    """
+    >>> select_sort_p([1, 4, 7, 2, 5, 8])
+    [1, 2, 4, 5, 7, 8]
+    """
+    count = len(nums)
+    for i in range(count):
+        m = i
+        for j in range(i+1, count):
+            if nums[m] > nums[j]:
+                m = j
+        nums[i], nums[m] = nums[m], nums[i]
+    return nums
+
+
+def bubble_sort(nums):
+    pass
+
+
+if __name__ == '__main__':
+    doctest.testmod()
+
