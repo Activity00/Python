6bc2ef0feaf89138d860a1bf7aba76930c70e621
武明辉 <1032662429@qq.com>
2019-01-08 19:05:22 +0800

    WIP: 10 convert image to webp

---
+++ Intrtoalgorithms/sort_test.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+
+"""
+@author: 武明辉 
+@time: 19-1-7 下午5:55
+"""
+import doctest
+import random
+import operator
+
+
+def insert_sort(nums):
+    """
+    >>> nums = random.sample(range(10), 10)
+    >>> target = sorted(nums)
+    >>> operator.eq(insert_sort(nums), target)
+    True
+    """
+    count = len(nums)
+    for i in range(1, count):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and key < nums[j]:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = key
+
+    return nums
+
+
+def insert_sort_d(nums):
+    """
+    >>> nums = random.sample(range(10), 10)
+    >>> target = sorted(nums,reverse=True)
+    >>> operator.eq(insert_sort_d(nums), target)
+    True
+    """
+    count = len(nums)
+    for i in range(1, count):
+        key = nums[i]
+        j = i-1
+        while j >= 0 and nums[j] < key:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = key
+
+    return nums
+
+
+def select_sort(nums):
+    """
+    >>> nums = random.sample(range(10), 10)
+    >>> target = sorted(nums,reverse=False)
+    >>> operator.eq(select_sort(nums), target)
+    True
+    """
+    count = len(nums)
+    for i in range(count):
+        for j in range(i+1, count):
+            if nums[j] < nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+    return nums
+
+
+if __name__ == '__main__':
+    nums = random.sample(range(10), 10)
+    print(nums)
+    target = sorted(nums, reverse=True)
+    print(select_sort(nums))
+    doctest.testmod()
