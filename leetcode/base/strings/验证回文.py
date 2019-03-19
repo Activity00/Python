c32c912867922c27f04ce34e0e2028b76249ef97
武明辉 <1032662429@qq.com>
2019-03-19 21:59:31 +0800

    WIP: 验证回文

--- leetcode/base/strings/验证回文.py
+++ leetcode/base/strings/验证回文.py
@@ -4,6 +4,8 @@
 @author: 武明辉 
 @time: 19-3-19 下午9:12
 """
+import re
+
 """
  验证回文字符串
 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
@@ -29,18 +31,24 @@ class Solution:
 
     def isPalindrome1(self, s: str) -> bool:
         """96 ms"""
-        t = [i.lower() for i in s if i.isalpha() or i.isdigit()]
+        t = [i for i in s if i.isalpha() or i.isdigit()]
         i = 0
         j = len(t) - 1
         m = len(t) // 2
         while i < m <= j:
-            if t[i] != t[j]:
+            if t[i].lower() != t[j].lower():
                 break
             i += 1
             j -= 1
         else:
             return True
         return False
+
+    def isPalindromel(self, s: str) -> bool:
+        """72ms"""
+        alphanumeric = re.sub("[^A-Za-z0-9]+", "", s).lower()
+        print(alphanumeric)
+        return alphanumeric == alphanumeric[::-1]
 
 
 if __name__ == '__main__':
