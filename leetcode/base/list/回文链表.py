1c589e98b2568aa28fa637a26b48ae8e0533a8fe
武明辉 <1032662429@qq.com>
2019-03-20 21:35:17 +0800

    WIP: 验证回文链表

--- leetcode/base/list/回文链表.py
+++ leetcode/base/list/回文链表.py
@@ -23,14 +23,35 @@ class ListNode:
 
 
 class Solution:
+
+    def reverse_link(self, head):
+        cur = head
+        pre = None
+        nxt = cur.next
+        while nxt:
+            cur.next = pre
+            pre = cur
+            cur = nxt
+            nxt = cur.next
+        cur.next = pre
+        return cur
+
     def isPalindrome(self, head: ListNode) -> bool:
         if not head or not head.next:
             return True
         if head.next.next is None:
             return head.val == head.next.val
 
-        fast = slow = head
-        while slow.next and fast.next.next:
+        fast = slow = q = head
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-        
+        p = self.reverse_link1(slow.next)
+        while p:
+            if p.val != q.val:
+                break
+            p = p.next
+            q = q.next
+        else:
+            return True
+        return False
