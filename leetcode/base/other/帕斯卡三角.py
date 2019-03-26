17daaef15181b5b1f9c69157df8da4a1f499060a
wuminghui <1032662429@qq.com>
2019-03-26 11:59:38 +0800

    WIP: 杨辉三角

--- leetcode/base/other/帕斯卡三角.py
+++ leetcode/base/other/帕斯卡三角.py
@@ -22,4 +22,10 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        pass
+        res = []
+        for i in range(0, numRows):
+            res.append([1] * (i + 1))
+            for j in range(1, i):
+                res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+        return res
+
