298f89e8b09258d2369578f97ad7a86145c63f53
wuminghui <1032662429@qq.com>
2019-03-27 16:16:48 +0800

    WIP: 堆

--- datastruct/堆.py
+++ datastruct/堆.py
@@ -8,9 +8,12 @@ import heapq
 
 if __name__ == '__main__':
     heap = []  # creates an empty heap
-    heapq.heappush(heap, 5)  # pushes a new item on the heap
+    heapq.heappush(heap, {'a': 1})  # pushes a new item on the heap
+
+    """
     item = heapq.heappop(heap)  # pops the smallest item from the heap
     item = heap[0]  # smallest item on the heap without popping it
     heapq.heapify(x)  # transforms list into a heap, in-place, in linear time
     item = heapq.heapreplace(heap, item)  # pops and returns smallest item, and adds
     # new item; the heap size is unchanged
+    """
