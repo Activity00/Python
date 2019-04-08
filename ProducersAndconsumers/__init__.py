1207f1f6f9fd6a4837a0d564dd60f938fdc803a3
wuminghui <1032662429@qq.com>
2019-04-08 09:31:55 +0800

    WIP: 2py3

--- ProducersAndconsumers/__init__.py
+++ ProducersAndconsumers/__init__.py
@@ -1,37 +1,38 @@
-
 import random
 from threading import Thread, Lock
 import time
 
-__all__=['Produces','Consume']
+__all__ = ['Produces', 'Consumer']
 
-queue=[]
-lock=Lock()
+queue = []
+lock = Lock()
+
 
 class Produces(Thread):
     def run(self):
-        nums=range(5)
+        nums = range(5)
         global queue
         while True:
-            num=random.choice(nums)
+            num = random.choice(nums)
             lock.acquire()
             queue.append(num)
-            print 'Produced',num
+            print('Produced', num)
             lock.release()
             time.sleep(random.random())
-            
+
+
 class Consumer(Thread):
     def run(self):
         global queue
         while True:
             lock.acquire()
             if not queue:
-                print 'Nothing in queue but consume ttryto consume'
-            num=queue.pop(0)
-            print 'Consume',num
+                print('Nothing in queue but consume ttryto consume')
+            num = queue.pop(0)
+            print('Consume', num)
             lock.release()
             time.sleep(random.random())
-        
+
 
 Produces().start()
 Consumer().start()
