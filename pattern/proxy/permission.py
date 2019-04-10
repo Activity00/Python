3f51c9a7e843e792f93bc14a2ec5a156d5b53361
wuminghui <1032662429@qq.com>
2019-04-10 10:01:28 +0800

    WIP: proxy pattern

---
+++ pattern/proxy/permission.py
@@ -0,0 +1,22 @@
+class SensitiveInfo:
+    def __init__(self):
+        self.users = ['nick', 'tom']
+
+    def read(self):
+        print(f'user info: {self.user}')
+
+    def add(self, user):
+        self.users.append(user)
+        print('add a user')
+
+
+class Info:
+    def __init__(self):
+        self.protected = SensitiveInfo()
+        self.screct = 'xxxx'
+
+    def read(self):
+        self.protected.read()
+
+    def add(self, user):
+        self.protected.add(user)
