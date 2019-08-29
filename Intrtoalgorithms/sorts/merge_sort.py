ca36ca0138bf8139f5e6be677b79f99466fdf877
wuminghui <1032662429@qq.com>
2019-08-29 16:22:49 +0800

    WIP: merge sort

--- Intrtoalgorithms/sorts/merge_sort.py
+++ Intrtoalgorithms/sorts/merge_sort.py
@@ -1,10 +1,6 @@
-import operator
-import random
-
-
 def merge(nums, p, q, r):
     print(f'merge {nums[p:q + 1]} {nums[q + 1:r + 1]}')
-    left = nums[p:q+1]
+    left = nums[p:q + 1]
     right = nums[q+1:r+1]
     left.append(float('inf'))
     right.append(float('inf'))
@@ -21,13 +17,6 @@ def merge(nums, p, q, r):
 
 
 def merge_sort(nums, p, r):
-    """
-    >>> nums = random.sample(range(10), 10)
-    >>> ret = sorted(nums)
-    >>> merge_sort(nums, 0, len(nums)-1)
-    >>> operator.eq(ret, nums)
-    True
-    """
     if p < r:
         q = (p + r) // 2
         merge_sort(nums, p, q)
@@ -36,13 +25,12 @@ def merge_sort(nums, p, r):
 
 
 def test():
-    # nums_list = [5, 2, 4, 7, 1, 3, 2, 6, 9]
-    nums_list = [5, 2, 4]
+    nums_list = [5, 2, 4, 7, 1, 3, 2, 6, 9, 10]
+    # nums_list = [5, 2, 4]
     merge_sort(nums_list, 0, len(nums_list) - 1)
     print(nums_list)
 
 
 if __name__ == '__main__':
-    # doctest.testmod()
     test()
 
