e9ef4d6c0c1d55f3743f10521c80ce1401b9b3cb
wuminghui <1032662429@qq.com>
2019-09-02 15:27:18 +0800

    WIP: three traverse

--- datastruct/pytree/pytree/utils.py
+++ datastruct/pytree/pytree/utils.py
@@ -54,49 +54,40 @@ def level_traversal(btree, func: Callable) -> None:
             queue.append(q.right)
 
 
-def in_order_traverse(node, func, is_ret=False):
-    if not node:
+def pre_order_traverse(btree, func):
+    if not btree:
         return
-    ret = [] if is_ret else None
-    tmp_node = node
+
+    stack = [btree.root]
+    while stack:
+        n = stack.pop()
+        func(n)
+        if n.right:
+            stack.append(n.right)
+        if n.left:
+            stack.append(n.left)
+
+
+def in_order_traverse(btree, func):
+    if not btree:
+        return
+    tmp_node = btree.root
     stack = []
     while tmp_node or stack:
         while tmp_node:
             stack.append(tmp_node)
             tmp_node = tmp_node.left
-        if stack:
-            n = stack.pop()
-            if n:
-                r = func(n)
-                if is_ret:
-                    ret.append(r)
-            if n.right:
-                tmp_node = n.right
-    return ret
 
-
-def pre_order_traverse(node, func, is_ret=False):
-    if not node:
-        return
-    ret = [] if is_ret else None
-    stack = [node]
-    while stack:
         n = stack.pop()
-        r = func(n)
-        if is_ret:
-            ret.append(r)
+        func(n)
         if n.right:
-            stack.append(n.right)
-        if n.left:
-            stack.append(n.left)
-    return ret
+            tmp_node = n.right
 
 
-def post_order_traverse(node, func, is_ret=False):
-    if not node:
+def post_order_traverse(btree, func):
+    if not btree:
         return
-    ret = [] if is_ret else None
-    stack1 = [node]
+    stack1 = [btree.root]
     stack2 = []
     while stack1:
         n = stack1.pop()
@@ -107,10 +98,7 @@ def post_order_traverse(node, func, is_ret=False):
         stack2.append(n)
     while stack2:
         n = stack2.pop()
-        r = func(n)
-        if is_ret:
-            ret.append(r)
-    return ret
+        func(n)
 
 
 
